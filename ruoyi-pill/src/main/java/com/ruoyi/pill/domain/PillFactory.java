7b8ccced8aa6a687c470bf73fb3507d44bc099c1
huazi <3056508050@qq.com>
2023-12-02 17:22:09 +0800

    1

--- ruoyi-pill/src/main/java/com/ruoyi/pill/domain/PillFactory.java
+++ ruoyi-pill/src/main/java/com/ruoyi/pill/domain/PillFactory.java
@@ -3,5 +3,5 @@ package com.ruoyi.pill.domain;
 import com.ruoyi.common.core.domain.BaseEntity;
 
 public class PillFactory extends BaseEntity {
-    private static final long serialVersionUID = 1L;
+
 }
